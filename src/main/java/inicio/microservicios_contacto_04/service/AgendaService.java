b0e6d9d51c1d1b7207ece149e945634dbc07618c
Chitiva09 <bermeocamilo12@gmail.com>
2024-08-10 20:34:39 -0500

    hice la interfaz de los servicios de la agenda, hice la implementacion de los servicios donde hago la logic de negocio o logica como tal del microservicio

---
+++ src/main/java/inicio/microservicios_contacto_04/service/AgendaService.java
@@ -0,0 +1,38 @@
+package inicio.microservicios_contacto_04.service;
+
+import java.util.List;
+
+import inicio.microservicios_contacto_04.model.Contacto;
+
+public interface AgendaService {
+
+Boolean agregarContacto (Contacto contacto);
+/*
+da la posibilidad de agregar contacto desde un objeto contacto pero con el boolean nos informa si
+se ha podido añadir o no, en caso de que ya exista no lo añade y devuelve falso y si se pudo añadir
+devolvera verdadero
+*Este método acepta un objeto de tipo Contacto como parámetro.
+ */
+List<Contacto> recuperarContactos();
+/*
+ * recuperarcontactos devuelve toda la lista de contactos por eso el return es un list
+ *  de la identidad contacto
+ */
+void actualizarContacto(Contacto contacto);
+/*
+ * no retorna nada, solo se ejecuta y ya, actualza contacto desde un objeto contacto
+ */
+Boolean eliminarContacto( int idContacto);
+/*
+ * elimina contacto con el idContacto, con el boolean nos da falso si no se ha podido eliminar
+ *  el contacto y verdadero cuando se ha podido eliminar el contacto con exito
+ */
+Contacto buacarContacto(int idContacto);
+/*
+ * busca contactos por la idContacto, retorna Contacto, porque al buscar por idContacto traera el 
+ * objeto contacto con todas sus propiedades 
+ * CHATGPT: Retorna un objeto Contacto con la información del contacto correspondiente al idContacto.
+ * Si no se encuentra el contacto, el retorno podría ser null
+ *  (aunque esto no está especificado en la interfaz). 
+ */
+}
---
+++ src/main/java/inicio/microservicios_contacto_04/service/AgendaServiceImpl.java
@@ -0,0 +1,65 @@
+package inicio.microservicios_contacto_04.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import inicio.microservicios_contacto_04.dao.AgendaDao;
+import inicio.microservicios_contacto_04.model.Contacto;
+
+/*
+ * como queremos que Spring instancie esta clase para que luego se inyecte donde se va a utilzar 
+ * que seran los controladores se utiliza la anotacion @Service
+ * -@Service es mas adecuada para una clase que contiene la logica de nogocio o logica como tal
+ *  del microservicio
+ */
+@Service
+public class AgendaServiceImpl implements AgendaService {
+
+    /*
+     *como vamos a utilizar los objetos de la clase AgendaDao dentro de la capa de servicios para 
+     *llamar a los metodos para que hagan las operaciones sobre la capa de persistencia, hay que inyectar 
+     *una implementacion de AgendaDao
+     *- declaramo la variable del tio de la interfaz en este caso es AgendaDao
+     */
+    @Autowired
+    AgendaDao dao;
+
+    @Override
+    public Boolean agregarContacto(Contacto contacto) {
+    
+        if (dao.recuperarContacto(contacto.getIdContacto())==null) {
+            dao.agregarContacto(contacto);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<Contacto> recuperarContactos() {
+        return dao.devolverContactos();  
+    }
+
+    @Override
+    public void actualizarContacto(Contacto contacto) {
+        if (dao.recuperarContacto(contacto.getIdContacto())!=null) {
+            dao.actualizarContacto(contacto);
+        }
+    }
+
+    @Override
+    public Boolean eliminarContacto(int idContacto) {
+        if (dao.recuperarContacto(idContacto)!=null) {
+            dao.eliminarContacto(idContacto);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Contacto buacarContacto(int idContacto) {
+        return dao.recuperarContacto(idContacto);
+    }
+
+}
